2b956aef4780b76177651e6c358883b2a90855fc
Ian T Butler <itb206@nyu.edu>
2017-09-12 15:02:15 -0400

    Bug fixes and new endpoints for request handling

--- packages/aura-be/src/db/graphql/schema/mutations.js
+++ packages/aura-be/src/db/graphql/schema/mutations.js
@@ -8,6 +8,7 @@ export default () => [
     reactivateTeam(nonce: String!, name: String!): JSON
     requestAddTeamMember( nonce: String!, url: String!, targetUser: String!, teamName: String! ): JSON
     resetPassword(nonce: String!, resetToken: String!, newPassword: String!): JSON
+    sendRequest(nonce: String!, teamName: String!, message: String!, state: JSON!): JSON
     signUpUser(
       firstName: String!,
       lastName: String!,
