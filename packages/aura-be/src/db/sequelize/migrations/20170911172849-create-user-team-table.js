a4fcd00970133f3996277fff16c588028ac8ba0e
Ian T Butler <itb206@nyu.edu>
2017-09-12 15:03:34 -0400

    Migrations and resolvers

---
+++ packages/aura-be/src/db/sequelize/migrations/20170911172849-create-user-team-table.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('UserTeams', {
+      tag: { type: Sequelize.UUID, allowNull: false, defaultValue: Sequelize.UUIDV4, unique: true},
+      active: { type: Sequelize.BOOLEAN, defaultValue: false },
+      activationNonce: { type: Sequelize.STRING, unique: true },
+      user_id: { type: Sequelize.INTEGER },
+      team_id: { type: Sequelize.INTEGER },
+      type: { type: Sequelize.ENUM, values: ['OWNER', 'ADMIN', 'MEMBER', 'GUEST'], allowNull: false },
+      created_at: { allowNull: false, type: Sequelize.DATE },
+      updated_at: { allowNull: false, type: Sequelize.DATE }
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('UserTeams');
+  }
+};
