abf31cf051a2856657aef1642335e9240a69d75d
cd10012 <ced361@nyu.edu>
2018-03-12 18:23:29 -0400

    Initial refactor removing scss and editing the css

--- packages/aura-fe/src/app/components/Chatbox/MessageList/MessageList.jsx
+++ packages/aura-fe/src/app/components/Chatbox/MessageList/MessageList.jsx
@@ -3,6 +3,7 @@ import './MessageList.css';
 import Message from '../Message/Message.jsx';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import styled from 'styled-components';
 
 export default class MessageList extends Component {
   render(props) {
@@ -10,12 +11,23 @@ export default class MessageList extends Component {
       return ( <Message key={index} show={message.show} author={message.author}>{message.message}</Message>);
     })
 
+    const Discussion = styled.ol`
+      list-style: none;
+      background: #e5e5e5;
+      margin: 0;
+      padding: 100px 100px 100px 100px;
+      font: 12px 'PT Mono', monospace;
+      height: 60%;
+      position: relative;
+      height: 95%;
+    `;
+
     return (
-      <ol className={"discussion"}>
+      <Discussion>
         <ReactCSSTransitionGroup transitionName="fade" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
           {messages}
         </ReactCSSTransitionGroup>
-      </ol>
+      </Discussion>
     );
   }
 }
